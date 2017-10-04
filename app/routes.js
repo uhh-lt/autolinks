d1b5f4b4bb7e5da54aa3130c4e81a674524e6da2
alvinrindra <alvinrindra@gmail.com>
2017-10-04 15:49:52 +0200

    Update dependencies and routes

---
+++ app/routes.js
@@ -0,0 +1,7 @@
+module.exports = function (app) {
+
+    // application -------------------------------------------------------------
+    app.get('*', function (req, res) {
+        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+};
