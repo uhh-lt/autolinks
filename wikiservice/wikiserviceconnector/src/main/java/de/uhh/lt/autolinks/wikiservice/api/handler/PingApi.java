228d2fc0f8b4193feb4f3cc27d8ece0a6e626db9
Steffen Remus <steremus@gmail.com>
2017-10-23 14:37:21 +0200

    added endpoint to register self at broker url

--- wikiservice/wikiserviceconnector/src/main/java/de/uhh/lt/autolinks/wikiservice/api/handler/PingApi.java
+++ wikiservice/wikiserviceconnector/src/main/java/de/uhh/lt/autolinks/wikiservice/api/handler/PingApi.java
@@ -5,22 +5,15 @@
  */
 package de.uhh.lt.autolinks.wikiservice.api.handler;
 
-import de.uhh.lt.autolinks.wikiservice.api.model.Error;
-
-import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import javax.validation.constraints.*;
-import javax.validation.Valid;
+import de.uhh.lt.autolinks.wikiservice.api.model.Error;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-10-20T13:51:53.035+02:00")
 
 @Api(value = "ping", description = "the ping API")
