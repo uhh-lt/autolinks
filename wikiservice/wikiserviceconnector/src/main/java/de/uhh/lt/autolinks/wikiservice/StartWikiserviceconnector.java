bd035002273cc2c8f2d2d52727530bc84d3e8b26
Steffen Remus <steremus@gmail.com>
2017-10-23 22:14:19 +0200

    swagger startup app

---
+++ wikiservice/wikiserviceconnector/src/main/java/de/uhh/lt/autolinks/wikiservice/StartWikiserviceconnector.java
@@ -0,0 +1,36 @@
+package de.uhh.lt.autolinks.wikiservice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackages = { "io.swagger", "de.uhh.lt.autolinks.wikiservice" })
+public class StartWikiserviceconnector implements CommandLineRunner {
+
+    @Override
+    public void run(String... arg0) throws Exception {
+        if (arg0.length > 0 && arg0[0].equals("exitcode")) {
+            throw new ExitException();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(StartWikiserviceconnector.class).run(args);
+    }
+
+    class ExitException extends RuntimeException implements ExitCodeGenerator {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public int getExitCode() {
+            return 10;
+        }
+
+    }
+}
