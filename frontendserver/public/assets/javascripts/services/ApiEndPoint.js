ab4fd2f56d1a5bcedd144e70b23dfb55dcd68f02
alvinrindra <alvinrindra@gmail.com>
2017-10-31 22:14:43 +0100

    ApiEndPoint init

---
+++ frontendserver/public/assets/javascripts/services/ApiEndPoint.js
@@ -0,0 +1,12 @@
+
+define([
+    'angular'
+], function (angular) {
+    'use strict';
+    angular.module('myApp.apiendpoint', [])
+        .factory('ApiEndPoint', ['$rootScope', function ($rootScope) {
+          
+            return {
+            };
+        }])
+});
