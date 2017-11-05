3485bc3d5ddca5e93d951f69fbbb1d87adf9583c
alvinrindra <alvinrindra@gmail.com>
2017-11-05 22:58:59 +0100

    Change frontend server port

--- frontendserver/server.js
+++ frontendserver/server.js
@@ -1,7 +1,7 @@
 // set up ======================================================================
 var express = require('express');
 var app = express(); 						// create our app w/ express
-var port = process.env.PORT || 8080; 				// set the port
+var port = process.env.PORT || 9000; 				// set the port
 
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/assets'));
