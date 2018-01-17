f63f05962cfe44cb383ddcc903fe81df2299e9d0
alvinrindra <alvinrindra@gmail.com>
2018-01-17 18:22:55 +0100

    update api/service/ endpoint

--- frontendserver/api/service/call.js
+++ frontendserver/api/service/call.js
@@ -1,61 +1,52 @@
-module.exports = function call(req, res) {
-    var request = require('request');
-    ﻿var config = require('config.json');
-
-    debugger;
-    var options = {
-      url: config.apiUrl + '/service/call',
-      method: 'POST',
-      headers: {
-         'Content-Type': 'application/json',
-         'Accept': 'application/json',
-         'authorization': 'Basic am9objpkb2U='
-       },
-      json: true,
-      body:
-      {
-        "service": "Wiki",
-        "version": "0.0.1",
-        "path": "/findarticles",
-        "method": "post",
-        "data": {
-          "focus": {
-            "offsets": [
-              {
-                "from": 0,
-                "length": 15
-              }
-            ]
-          },
-          "context": {
-            "text": req.body.text,
-            "source": "string",
-            "lang": "string",
-            "availabletypes": [
-              "string"
-            ],
-            "annotations": [
-              {
-                "type": "string",
-                "doffset": {
-                  "offsets": [
-                    {
-                      "from": 0,
-                      "length": 0
-                    }
-                  ]
-                },
-                "properties": {},
-                "analyzer": "string"
-              }
-            ]
-          }
+module.exports = function(url, text) {
+  return {
+    url: url + '/service/call',
+    method: 'POST',
+    headers: {
+       'Content-Type': 'application/json',
+       'Accept': 'application/json',
+       'authorization': 'Basic am9objpkb2U='
+     },
+    json: true,
+    body:
+    {
+      "service": "Wiki",
+      "version": "0.0.1",
+      "path": "/findarticles",
+      "method": "post",
+      "data": {
+        "focus": {
+          "offsets": [
+            {
+              "from": 0,
+              "length": 15
+            }
+          ]
+        },
+        "context": {
+          "text": text,
+          "source": "string",
+          "lang": "string",
+          "availabletypes": [
+            "string"
+          ],
+          "annotations": [
+            {
+              "type": "string",
+              "doffset": {
+                "offsets": [
+                  {
+                    "from": 0,
+                    "length": 0
+                  }
+                ]
+              },
+              "properties": {},
+              "analyzer": "string"
+            }
+          ]
         }
       }
     }
-
-    var req = request(options, function (error, response, body) {
-      debugger;
-      res.send(body);
-    });
+  }
 }
--- frontendserver/api/service/services.js
+++ frontendserver/api/service/services.js
@@ -3,80 +3,23 @@ var request = require('request');
 var express = require('express');
 var router = express.Router();
 var serviceCall = require('./call');
+var serviceList = require('./list');
 
-router.get('/listServices', listServices);
+router.get('/listServices', list);
 router.post('/call', call);
 
 module.exports = router;
 
-function listServices(req, res){
-    var options = {
-          url: config.apiUrl + '/service/listServices',
-          method: 'GET',
-          headers: {
-             'Content-Type': 'application/json',
-             'accept': 'application/json'
-           },
-          json: true,
-        }
-    var req = request(options, function (error, response, body) {
+function list(req, res){
+    const options = serviceList(config.apiUrl);
+    request(options, function (error, response, body) {
       res.send(body);
     });
 }
 
 function call(req, res) {
-    var options = {
-      url: config.apiUrl + '/service/call',
-      method: 'POST',
-      headers: {
-         'Content-Type': 'application/json',
-         'Accept': 'application/json',
-         'authorization': 'Basic am9objpkb2U='
-       },
-      json: true,
-      body:
-      {
-        "service": "Wiki",
-        "version": "0.0.1",
-        "path": "/findarticles",
-        "method": "post",
-        "data": {
-          "focus": {
-            "offsets": [
-              {
-                "from": 0,
-                "length": 15
-              }
-            ]
-          },
-          "context": {
-            "text": req.body.text,
-            "source": "string",
-            "lang": "string",
-            "availabletypes": [
-              "string"
-            ],
-            "annotations": [
-              {
-                "type": "string",
-                "doffset": {
-                  "offsets": [
-                    {
-                      "from": 0,
-                      "length": 0
-                    }
-                  ]
-                },
-                "properties": {},
-                "analyzer": "string"
-              }
-            ]
-          }
-        }
-      }
-    }
-
-    var req = request(options, function (error, response, body) {
+    const options = serviceCall(config.apiUrl, req.body.text);
+    request(options, function (error, response, body) {
       res.send(body);
     });
 }
