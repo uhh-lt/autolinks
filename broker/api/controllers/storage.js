61a4a1007d57a1bd487082a00c8648a23c22daed
Steffen Remus <steremus@gmail.com>
2018-01-20 22:34:55 +0100

    resource management

--- broker/api/controllers/storage.js
+++ broker/api/controllers/storage.js
@@ -21,38 +21,20 @@ module.exports.info = function(req, res, next) {
 
 module.exports.read = function(req, res, next) {
   auth.handle_authenticated_request(req, res, function(user){
-    res.header('Content-Type', 'application/json; charset=utf-8');
-    const key = req.swagger.params.key.value;
-    // console.log(Buffer.from("Hello World").toString('base64'));
-    // console.log(Buffer.from("SGVsbG8gV29ybGQ=", 'base64').toString('ascii'));
-    storage.read(user.name, key, function(err, result){
-      if(err) {
-        return Exception.fromError(err, 'Could not read data.').log(logger.warn).handleResponse(res).end(next);
-      }
-      if(result) {
-        res.json(result);
-      } else {
-        res.status(204);
-      }
-      res.end(next);
-    });
+    new Exception('NOT IMPLEMENTED', 'not yet implemented').handleResponse(res).end(next);
   });
+
 };
 
 module.exports.write = function(req, res, next) {
   auth.handle_authenticated_request(req, res, function(user){
-    res.header('Content-Type', 'application/json; charset=utf-8');
-    const kvp = req.swagger.params.kvp.value;
-    const key = kvp.key;
-    const value = kvp.value;
-    // console.log(Buffer.from("Hello World").toString('base64'));
-    // console.log(Buffer.from("SGVsbG8gV29ybGQ=", 'base64').toString('ascii'));
-    storage.write(user.name, key, value, function(err){
-      if(err) {
-        return Exception.fromError(err, 'Could not write data.').log(logger.warn).handleResponse(res).end(next);
-      }
-      res.end(next);
-    });
+    new Exception('NOT IMPLEMENTED', 'not yet implemented').handleResponse(res).end(next);
+  });
+};
+
+module.exports.editresource = function(req, res, next) {
+  auth.handle_authenticated_request(req, res, function(user) {
+    new Exception('NOT IMPLEMENTED', 'not yet implemented').handleResponse(res).end(next);
   });
 };
 
