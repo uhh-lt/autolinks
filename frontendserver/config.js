66fd292620473e97404824fed55ffccfc2f05a9c
alvinrindra <alvinrindra@gmail.com>
2018-06-18 18:52:01 +0200

    small fix process.env

--- frontendserver/config.js
+++ frontendserver/config.js
@@ -1,7 +1,7 @@
 module.exports = function() {
   return {
     "connectionString": "mongodb://localhost:27017/xxx-xxx-xxx",
-    "apiUrl": process.env.BROKER_URL || 'http://ltdemos.informatik.uni-hamburg.de:8093' || 'http://broker:10010',
+    "apiUrl": process.env.BROKER_URL || 'http://broker:10010',
     "brokerEndPoint": "services",
     "secret": "REPLACE THIS WITH YOUR OWN SECRET, IT CAN BE ANY STRING"
   }
