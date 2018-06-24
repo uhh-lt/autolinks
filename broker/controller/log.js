21b49e8834ad15d67107184b181582bb08a18c9b
Steffen Remus <steremus@gmail.com>
2018-06-24 12:31:18 +0200

    - updated node version; -updated node packages

--- broker/controller/log.js
+++ broker/controller/log.js
@@ -11,43 +11,65 @@ function getLabel(callingModule) {
   return parts[parts.length - 2] + '/' + parts.pop();
 }
 
+const myLogFormatter = winston.format.printf(info => {
+  let colorcode = '';
+  let resetcolorcode = '';
+  const colorize = info.level.startsWith('\u001b[');
+  if(colorize) {
+    colorcode = info.level.substring(0, info.level.search('m') + 1);
+    resetcolorcode = '\u001b[0;39m';
+  }
+  return `${info.level} ${colorcode}${info.timestamp}${resetcolorcode} ${colorcode}[${info.label}]:${resetcolorcode} ${info.message}`;
+});
+
 module.exports = function(callingModule) {
 
+  // check if callingModule is actually a module
   if( callingModule === Object(callingModule) ){
     const aModulesLabel = getLabel(callingModule);
-    return new (winston.Logger)({
+    const logger = winston.createLogger({
+      format: winston.format.combine(
+        winston.format.splat(),
+        winston.format.label({ label: aModulesLabel }),
+        winston.format.timestamp(),
+        myLogFormatter
+      ),
       transports: [
-        new (winston.transports.Console)({
-          level: loglevel,
-          prettyPrint: false,
-          colorize: true,
-          timestamp: true,
-          label: aModulesLabel
-        }),
-        new (winston.transports.File)({
+        new winston.transports.File({
           level: 'debug',
           maxsize:'10000000',
           maxFiles: 10,
           filename: path.resolve(global.__datadir && path.join(global.__datadir, 'server.log') || 'server.log'),
           handleExceptions: true,
           humanReadableUnhandledException: true,
-          timestamp: true,
-          json: false,
-          prettyPrint: false,
-          label: aModulesLabel,
         })
       ]
     });
+    logger.add(new winston.transports.Console({
+      level: loglevel,
+      format: winston.format.combine(
+        winston.format.colorize({ all: true }),
+        winston.format.splat(),
+        winston.format.label({ label: aModulesLabel }),
+        winston.format.timestamp(),
+        myLogFormatter
+      )
+    }));
+
+    return logger;
   }
-  // else
-  return new (winston.Logger)({
+  // if callingModule is not a module use simply the string and no file logger
+  return winston.createLogger({
     transports: [
-      new (winston.transports.Console)({
+      new winston.transports.Console({
         level: loglevel,
-        prettyPrint: false,
-        colorize: true,
-        timestamp: true,
-        label: callingModule
+        format: winston.format.combine(
+          winston.format.colorize({ all: true }),
+          winston.format.splat(),
+          winston.format.label({ label: callingModule }),
+          winston.format.timestamp(),
+          myLogFormatter
+        )
       })
     ]
   });
