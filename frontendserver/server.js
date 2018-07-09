009818ba063efabcba4d311fa184196aadcf8dcf
alvinrindra <alvinrindra@gmail.com>
2018-07-09 16:11:00 +0200

    Update autolinks logo and favicon

--- frontendserver/server.js
+++ frontendserver/server.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
 app.use(session({ secret: config().secret, resave: false, saveUninitialized: true }));
+app.use( express.static( "views" ) );
 
 app.use(fileUpload());
 
