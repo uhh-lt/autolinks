de1434aab48c5963a810fe4da0151b6e79a7ef3d
Steffen Remus <steremus@gmail.com>
2018-11-18 04:22:28 +0100

    more sparql queries

--- services/sparql/api/controllers/wikidata.js
+++ services/sparql/api/controllers/wikidata.js
@@ -6,8 +6,8 @@ const
   ServiceParameter = require('../../../../broker/model/ServiceParameter').model,
   Exception = require('../../../../broker/model/Exception').model;
 
-module.exports.wikidata = function(req, res, next) {
 
+function run(req, res, next, textpromise) {
   return ServiceParameter.fromRequestPromise(req).then(
     serviceParameter => {
       // get the text for the offset
@@ -18,9 +18,8 @@ module.exports.wikidata = function(req, res, next) {
         return res.header('Content-Type', 'text/plain; charset=utf-8').end(text, next);
       }
 
-      // query wikidata
-      return wd.search(text)
-        .then(wd.query_sparql)
+      // run promise
+      return textpromise(text)
         .then(
           resource => res.json(resource).end(next),
           err => {
@@ -32,5 +31,30 @@ module.exports.wikidata = function(req, res, next) {
     },
     err => Exception.handleErrorResponse(err, res).end(next)
   );
+}
+
+module.exports.wikidata = function(req, res, next) {
+  return run(req, res, next, (text) => wd.search(text).then(wd.query_sparql).then(r => {r.metadata.label = `Wikidata '${text}'.`; return r; }));
 };
+
+module.exports.wikidataObjects = function(req, res, next) {
+  return run(req, res, next, (text) => wd.search(text).then(wd.objects).then(r => {r.metadata.label = `Wikidata objects '${text}'.`; return r; }));
+};
+
+module.exports.wikidataSubjects = function(req, res, next) {
+  return run(req, res, next, (text) => wd.search(text).then(wd.subjects).then(r => {r.metadata.label = `Wikidata subjects '${text}'.`; return r; }));
+};
+
+module.exports.wikidataLimit = function(req, res, next) {
+  return run(req, res, next, (text) => wd.search(text, 1).then(h => wd.query_sparql(h)).then(r => {r.metadata.label = `Wikidata '${text}' (limit 1)`; return r; }));
+};
+
+module.exports.wikidataObjectsLimit10 = function(req, res, next) {
+  return run(req, res, next, (text) => wd.search(text, 1).then(h => wd.objects(h, 10)).then(r => {r.metadata.label = `Wikidata objects '${text}' (limit 1|10)`; return r; }));
+};
+
+module.exports.wikidataSubjectsLimit10 = function(req, res, next) {
+  return run(req, res, next, (text) => wd.search(text, 1).then(h => wd.subjects(h, 10)).then(r => {r.metadata.label = `Wikidata subjects '${text}' (limit 1|10)`; return r; }));
+};
+
 
--- services/sparql/controller/Wikidata.js
+++ services/sparql/controller/Wikidata.js
@@ -6,8 +6,6 @@ const
   Resource = require('../../../broker/model/Resource').model,
   log = require('../../../broker/controller/log')(module);
 
-const query = 'B-cell';
-
 function findhit(hits, uri) {
   return hits.findIndex(hit => hit.concepturi === uri);
 }
@@ -16,10 +14,10 @@ function safeget(p, o, def=null) {
   return p.reduce((xs, x) => (xs && xs[x]) ? xs[x] : def, o);
 }
 
-module.exports.search = function(query) {
+module.exports.search = function(query, limit=3) {
   return Promise.resolve(query)
     .then(() => {
-      const url = 'https://www.wikidata.org/w/api.php?format=json&limit=2&action=wbsearchentities&language=en&type=item&search=' + encodeURIComponent( query );
+      const url = `https://www.wikidata.org/w/api.php?format=json&limit=${limit}&action=wbsearchentities&language=en&type=item&search=` + encodeURIComponent( query );
       return fetcher( url, { headers: { 'Accept': 'application/json' } } );
     })
     .then( body => body.json() ).then( json => {
@@ -101,10 +99,116 @@ GROUP BY ?item ?itemLabel ?itemDescription ?article`, fullUrl = endpointUrl + en
         return triples;
       });
       const flatResourcelist = resourcelist.reduce((x,y) => x.concat(y), []);
-      return Resource.fromValue(flatResourcelist, { label: `Wikidata '${query}'`  });
+      return Resource.fromValue(flatResourcelist);
     })
     .catch(e => {
       log.error(e);
-      return Resource.fromValue([], { label: `Wikidata '${query}'.` });
+      return Resource.fromValue([]);
+    });
+};
+
+
+module.exports.objects = function(hits, limit=10000) {
+  return Promise.resolve(hits)
+    .then(() => {
+      const concepts = hits.map( result => result.id );
+      log.debug(`found ${concepts}`);
+      const wdconcepts = concepts.map(concept => `wd:${concept}`).join(' ');
+      const endpointUrl = 'https://query.wikidata.org/sparql?query=',
+        sparqlQuery = `
+SELECT DISTINCT
+  ?item ?itemLabel ?itemDescription ?prop ?propLabel ?propDescription ?obj ?objLabel ?objDescription
+WHERE { 
+  hint:Query hint:optimizer 'None' .
+  {	
+    VALUES ?item { ${wdconcepts} } .
+    ?item ?pred ?obj .
+    ?prop ?ref ?pred .
+    ?prop a wikibase:Property .
+    ?prop rdfs:label ?propLabel .
+  }
+  ?item rdfs:label ?itemLabel .
+  ?obj rdfs:label ?objLabel .
+  FILTER (LANG(?itemLabel) = 'en') .
+  FILTER (LANG(?objLabel) = 'en') .
+  FILTER (LANG(?propLabel) = 'en' ) .
+  OPTIONAL { ?item schema:description ?itemDescription FILTER (lang(?itemDescription) = 'en' ) . }
+  OPTIONAL { ?obj schema:description ?objDescription FILTER (lang(?objDescription) = 'en' ) . }
+  OPTIONAL { ?prop schema:description ?propDescription FILTER (lang(?propDescription) = 'en' ) . }
+} LIMIT ${limit}
+`, fullUrl = endpointUrl + encodeURIComponent( sparqlQuery );
+      return fetcher( fullUrl, { headers: { 'Accept': 'application/sparql-results+json' } } );
+    })
+    .then( body => body.json() )
+    .then( json => {
+      const { head: { vars }, results } = json;
+      // prepare results
+      const resourcelist =  results.bindings.map((result, i) => {
+        const item = safeget(['item','value'], result);
+        log.debug(`${i} -- ${item}`);
+        const itemresource = Resource.fromValue( item, { label:safeget(['itemLabel', 'value'], result, item), description:safeget(['itemDescription','value'], result, '') } );
+        const predresource = Resource.fromValue( safeget(['prop','value'], result), { label:safeget(['propLabel', 'value'], result, item), description:safeget(['propDescription','value'], result, '') } );
+        const objresource = Resource.fromValue( safeget(['obj','value'], result), { label:safeget(['objLabel', 'value'], result, item), description:safeget(['objDescription','value'], result, '') } );
+        const tripleresource = Resource.fromValue(new Triple(itemresource, predresource, objresource));
+        return tripleresource;
+      });
+      return Resource.fromValue(resourcelist);
+    })
+    .catch(e => {
+      log.error(e);
+      return Resource.fromValue([]);
+    });
+};
+
+
+module.exports.subjects = function(hits, limit=10000) {
+  return Promise.resolve(hits)
+    .then(() => {
+      const concepts = hits.map( result => result.id );
+      log.debug(`found ${concepts}`);
+      const wdconcepts = concepts.map(concept => `wd:${concept}`).join(' ');
+      const endpointUrl = 'https://query.wikidata.org/sparql?query=',
+        sparqlQuery = `
+SELECT DISTINCT
+  ?item ?itemLabel ?itemDescription ?prop ?propLabel ?propDescription ?subj ?subjLabel ?subjDescription
+WHERE { 
+  hint:Query hint:optimizer 'None' .
+  {	
+    VALUES ?item { ${wdconcepts} } .
+    ?subj ?pred ?item .
+    ?prop ?ref ?pred .
+    ?prop a wikibase:Property .
+    ?prop rdfs:label ?propLabel .
+  }
+  ?item rdfs:label ?itemLabel .
+  ?subj rdfs:label ?subjLabel .
+  FILTER (LANG(?itemLabel) = 'en') .
+  FILTER (LANG(?subjLabel) = 'en') .
+  FILTER (LANG(?propLabel) = 'en' ) .
+  OPTIONAL { ?item schema:description ?itemDescription FILTER (lang(?itemDescription) = 'en' ) . }
+  OPTIONAL { ?subj schema:description ?subjDescription FILTER (lang(?subjDescription) = 'en' ) . }
+  OPTIONAL { ?prop schema:description ?propDescription FILTER (lang(?propDescription) = 'en' ) . }
+} LIMIT ${limit}
+`, fullUrl = endpointUrl + encodeURIComponent( sparqlQuery );
+      return fetcher( fullUrl, { headers: { 'Accept': 'application/sparql-results+json' } } );
+    })
+    .then( body => body.json() )
+    .then( json => {
+      const { head: { vars }, results } = json;
+      // prepare results
+      const resourcelist =  results.bindings.map((result, i) => {
+        const item = safeget(['item','value'], result);
+        log.debug(`${i} -- ${item}`);
+        const itemresource = Resource.fromValue( item, { label:safeget(['itemLabel', 'value'], result, item), description:safeget(['itemDescription','value'], result, '') } );
+        const predresource = Resource.fromValue( safeget(['prop','value'], result), { label:safeget(['propLabel', 'value'], result, item), description:safeget(['propDescription','value'], result, '') } );
+        const subjresource = Resource.fromValue( safeget(['subj','value'], result), { label:safeget(['subjLabel', 'value'], result, item), description:safeget(['subjDescription','value'], result, '') } );
+        const tripleresource = Resource.fromValue(new Triple(subjresource, predresource, itemresource));
+        return tripleresource;
+      });
+      return Resource.fromValue(resourcelist);
+    })
+    .catch(e => {
+      log.error(e);
+      return Resource.fromValue([]);
     });
 };
